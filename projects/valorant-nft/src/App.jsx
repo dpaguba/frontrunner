21e3d34c8a40c55b054cb92a71d7890438876c55
Dmytro Pahuba <dpaguba@gmail.com>
2023-02-19 15:41:03 +0100

    feat: add the app shell and dark background

---
+++ projects/valorant-nft/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NFTMarketplace from "./NFTMarketplace";
+
+function App() {
+  return (
+    <div className="App">
+      <NFTMarketplace />
+    </div>
+  );
+}
+
+export default App;

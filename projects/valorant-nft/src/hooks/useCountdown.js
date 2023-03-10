2489840d506bdb8143854019083eac859b90f4ed
Dmytro Pahuba <dpaguba@gmail.com>
2023-03-10 19:16:35 +0100

    feat: add the countdown hook

---
+++ projects/valorant-nft/src/hooks/useCountdown.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export const useCountdown = (initialTime) => {
+    const [timeLeft, setTimeLeft] = useState(initialTime);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(prev => {
+                if (prev.seconds > 0) {
+                    return { ...prev, seconds: prev.seconds - 1 };
+                } else if (prev.minutes > 0) {
+                    return { ...prev, minutes: prev.minutes - 1, seconds: 59 };
+                } else if (prev.hours > 0) {
+                    return { ...prev, hours: prev.hours - 1, minutes: 59, seconds: 59 };
+                }
+                return prev;
+            });
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
+
+    return timeLeft;
+};

e79de2b7acd7e5cb8c612294a75bd4db1d28c1de
Dmytro Pahuba <dpaguba@gmail.com>
2023-06-20 22:26:22 +0200

    feat: wire up react router

---
+++ projects/velocity/src/App.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import CaseStudies from './pages/CaseStudies'
+import Contact from './pages/Contact'
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
+function App() {
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/case-studies" element={<CaseStudies />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+
+export default App
